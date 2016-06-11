fbb96c3e519e71586c3a08bcd08b87389e8227c3
akashrock <akash955954@gmail.com>
2016-06-11 11:02:50 +0530

    readfile
    another js file

---
+++ module.js
@@ -0,0 +1,10 @@
+var sayhello=function()
+{
+	console.log("hello");
+}
+var saygood=function()
+{
+	console.log("hellogood");
+}
+exports.sayhello=sayhello;
+exports.hellogood=saygood;
---
+++ mymodule.js
@@ -0,0 +1,6 @@
+var moudule=require("./module")
+{
+	console.log("./module")
+}
+moudule.sayhello();
+moudule.hellogood();
