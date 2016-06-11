60f2a39eb71a107d78c5a6f277df6a56ddf9c03b
akashrock <akash955954@gmail.com>
2016-06-11 14:17:28 +0530

    server start

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-var http=require('http')
-var server=http.createServer(function(req,res) {
-res.writeHead(200,{"contentType":"text/TML"})
-var Name="Suneel";
-var Dept="MCA";
-var email="suneel50@gmail.com";
-res.write("<h2>Name:</h2>" +
-	Name+"<br>"+"<h2>Dept:</h2>"+Dept+"<br>"+"<h2>email</h2>"+email)
+var express =require('express')
+var app = express()
+app.get('/',function(req,res)
+{
+res.writeHead(200)
+res.write("server is create")
+res.end()
 })
-server.listen(9090)
+app.listen(8080)
