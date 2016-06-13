07418558f0bcd5065b248d794088c8e9d85e8bba
akashrock <akash955954@gmail.com>
2016-06-13 15:11:36 +0530

    a

--- app.js
+++ app.js
@@ -1,13 +1,104 @@
-var express= require("express")
-var app=express();
-app.get('/',function(req,res)
-{
-	var myarr=["red","green","blue"]
-	res.render('login.ejs',
-	{
-		temp:myarr
-	});
-	res.end();
-})
-app.listen(8080)
-console.log("hello ashish");
+
+var express  = require('express'),
+   mongoose = require('mongoose'),
+   bodyParser = require('body-parser'),
+
+    // Mongoose Schema definition
+    Schema = new mongoose.Schema({
+      studentId       : String, 
+      name    : String,
+      department: String,
+      year    : String , 
+     roll     : number,
+      isPassedOut    : Boolean    
+    }),
+
+    User = mongoose.model('User', Schema);
+
+
+    mongoose.connect('mongodb://ash:1234@ds013574.mlab.com:13574/ashish_db')
+
+
+    var app = express()
+    
+    app.use(bodyParser.json()); // get information from html forms
+    app.use(bodyParser.urlencoded({extended: true}));
+ 
+
+  app.get('/api', function (req, res) {
+    res.json(200, {msg: 'OK' });
+  })
+
+  app.get('/api/users', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#query_Query-find
+    User.find({}, function ( err, users ){
+      res.status(200).json(users);
+    });
+  })
+
+  app.post('/api/user', function (req, res) {
+        var user = new User(
+        {
+        StudentId : req.body.StudentId,
+        name : req.body.name,
+        department : req.body.department,
+        year : req.body.year,
+        isPassedOut : req.body.isPassedOut
+        }
+    );
+  
+    // http://mongoosejs.com/docs/api.html#model_Model-save
+    user.save(function (err, data) {
+        if(!err && data){
+            console.log(data)
+            res.status(200).json(data)
+        } else {
+            console.log(err)
+        }
+      
+    });
+  })
+
+  app.delete('/api/users', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#query_Query-remove
+    User.remove({ isPassedOut: true }, function ( err ) {
+      res.json(200, {msg: 'OK'});
+    });
+  })
+
+  app.get('/api/users/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    User.findById( req.params.id, function ( err, todo ) {
+      res.json(200, todo);
+    });
+  })
+
+  app.put('/api/users/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    User.findById( req.params.id, function ( err, user ) {
+      user.isPassedOut = req.body.completed;
+      // http://mongoosejs.com/docs/api.html#model_Model-save
+      todo.save( function ( err, data ){
+          if(!err && data){
+           res.status(200).json(data)
+          } else {
+              console.log(err)
+          }
+       
+      });
+    });
+  })
+
+  app.delete('/api/users/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    User.findById( req.params.id, function ( err, user ) {
+      // http://mongoosejs.com/docs/api.html#model_Model.remove
+      user.remove( function ( err, user ){
+           res.status(200, {msg: 'OK'})
+      });
+    });
+  })
+
+  app.listen(1338);
+console.log('Magic happens on port 1338');
+
